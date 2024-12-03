83e0ceb034683507250e342223f7514413f8e3fd
shaflyh <shafly2000@gmail.com>
2024-12-03 13:21:39 +0700

    fix: error in new pom

--- src/main/java/com/hand/demo/DemoApplication.java
+++ src/main/java/com/hand/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.hand.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,7 +9,8 @@ import io.choerodon.resource.annoation.EnableChoerodonResourceServer;
 @EnableChoerodonResourceServer
 @EnableDiscoveryClient
 @SpringBootApplication
-
+@MapperScan({"com.hand.demo.infra.mapper", "org.hzero.boot.admin.translate.mapper",
+        "org.hzero.boot.imported.infra.mapper"})
 public class DemoApplication {
 
     public static void main(String[] args) {
