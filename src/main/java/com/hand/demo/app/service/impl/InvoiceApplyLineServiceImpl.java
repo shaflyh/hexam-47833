41c323ee6121eb4432b6a65f4a65586b587b6c90
shaflyh <shafly2000@gmail.com>
2024-12-05 09:08:08 +0700

    feat: add invoice line create validation and calculation

--- src/main/java/com/hand/demo/app/service/impl/InvoiceApplyLineServiceImpl.java
+++ src/main/java/com/hand/demo/app/service/impl/InvoiceApplyLineServiceImpl.java
@@ -1,6 +1,10 @@
 package com.hand.demo.app.service.impl;
 
+import com.hand.demo.domain.entity.InvoiceApplyHeader;
+import com.hand.demo.domain.repository.InvoiceApplyHeaderRepository;
+import com.hand.demo.infra.constant.ErrorCodeConst;
 import io.choerodon.core.domain.Page;
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.mybatis.pagehelper.PageHelper;
 import io.choerodon.mybatis.pagehelper.domain.PageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Service;
 import com.hand.demo.domain.entity.InvoiceApplyLine;
 import com.hand.demo.domain.repository.InvoiceApplyLineRepository;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,11 +27,14 @@ import java.util.stream.Collectors;
 @Service
 public class InvoiceApplyLineServiceImpl implements InvoiceApplyLineService {
     @Autowired
-    private InvoiceApplyLineRepository invoiceApplyLineRepository;
+    private InvoiceApplyLineRepository lineRepository;
+
+    @Autowired
+    private InvoiceApplyHeaderRepository headerRepository;
 
     @Override
     public Page<InvoiceApplyLine> selectList(PageRequest pageRequest, InvoiceApplyLine invoiceApplyLine) {
-        return PageHelper.doPageAndSort(pageRequest, () -> invoiceApplyLineRepository.selectList(invoiceApplyLine));
+        return PageHelper.doPageAndSort(pageRequest, () -> lineRepository.selectList(invoiceApplyLine));
     }
 
     @Override
@@ -34,8 +43,62 @@ public class InvoiceApplyLineServiceImpl implements InvoiceApplyLineService {
                 invoiceApplyLines.stream().filter(line -> line.getApplyLineId() == null).collect(Collectors.toList());
         List<InvoiceApplyLine> updateList =
                 invoiceApplyLines.stream().filter(line -> line.getApplyLineId() != null).collect(Collectors.toList());
-        invoiceApplyLineRepository.batchInsertSelective(insertList);
-        invoiceApplyLineRepository.batchUpdateByPrimaryKeySelective(updateList);
+        updateInvoiceLine(updateList);
+        insertInvoiceLine(insertList);
+    }
+
+    private void updateInvoiceLine(List<InvoiceApplyLine> updateList) {
+        if (!updateList.isEmpty()) {
+            // Update invoice lines validation
+            updateInvoiceLineValidation(updateList);
+            // Invoice lines calculation
+            List<InvoiceApplyLine> invoiceApplyLines = invoiceLineCalculation(updateList);
+            // Update invoice lines
+            lineRepository.batchUpdateByPrimaryKeySelective(invoiceApplyLines);
+        }
+    }
+
+    private void insertInvoiceLine(List<InvoiceApplyLine> insertList) {
+        if (!insertList.isEmpty()) {
+            // Invoice lines calculation
+            List<InvoiceApplyLine> invoiceApplyLines = invoiceLineCalculation(insertList);
+            // Insert new invoice lines
+            lineRepository.batchInsertSelective(invoiceApplyLines);
+        }
+    }
+
+    private void updateInvoiceLineValidation(List<InvoiceApplyLine> updateList) {
+        // Check if invoice apply header exist in database and not deleted
+        for (InvoiceApplyLine line : updateList) {
+            InvoiceApplyHeader invoiceApplyHeader = new InvoiceApplyHeader();
+            invoiceApplyHeader.setApplyHeaderId(line.getApplyHeaderId());
+            InvoiceApplyHeader header = headerRepository.selectOne(invoiceApplyHeader);
+            if (header == null) {
+                throw new CommonException(ErrorCodeConst.INVOICE_NOT_EXIST, line.getApplyHeaderId());
+            } else if (header.getDelFlag() == 1) {
+                throw new CommonException(ErrorCodeConst.INVOICE_DELETED, line.getApplyHeaderId());
+            }
+        }
+    }
+
+    private List<InvoiceApplyLine> invoiceLineCalculation(List<InvoiceApplyLine> invoiceApplyLines) {
+        // exclude_tax_amount, tax_amount, and total_amount calculation
+        for (InvoiceApplyLine line : invoiceApplyLines) {
+            // Fetch data from input
+            BigDecimal unitPrice = line.getUnitPrice() != null ? line.getUnitPrice() : BigDecimal.ZERO;
+            BigDecimal quantity = line.getQuantity() != null ? line.getQuantity() : BigDecimal.ZERO;
+            BigDecimal taxRate = line.getTaxRate() != null ? line.getTaxRate() : BigDecimal.ZERO;
+            BigDecimal taxRatePercent = taxRate.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+            // Calculation
+            BigDecimal totalAmount = unitPrice.multiply(quantity); // total_amount = unit_price * quantity
+            BigDecimal taxAmount = totalAmount.multiply(taxRatePercent); // tax_amount = total_amount * tax_rate
+            BigDecimal excludeTaxAmount = totalAmount.subtract(taxAmount); // = total_amount – tax_amount
+            // Set the data
+            line.setTotalAmount(totalAmount);
+            line.setTaxAmount(taxAmount);
+            line.setExcludeTaxAmount(excludeTaxAmount);
+        }
+        return invoiceApplyLines;
     }
 }
 
--- src/main/java/com/hand/demo/infra/constant/ErrorCodeConst.java
+++ src/main/java/com/hand/demo/infra/constant/ErrorCodeConst.java
@@ -7,4 +7,5 @@ package com.hand.demo.infra.constant;
 public class ErrorCodeConst {
     public static final String INVOICE_VALUE_INVALID = "hexam-47833.invoice.invalid_value";
     public static final String INVOICE_NOT_EXIST = "hexam-47833.invoice.not_exist";
+    public static final String INVOICE_DELETED = "hexam-47833.invoice.deleted";
 }
